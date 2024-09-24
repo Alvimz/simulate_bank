9eabaf62ff11552006169c167afaeceb249a17f3
Alvimz <gg_alvim@hotmail.com>
2024-09-24 15:30:52 -0300

    Delete transitions.py

--- transitions.py
+++
@@ -1,8 +0,0 @@
-class Transitions:
-    def __init__(self) -> None:
-        self.__transitions = list()
-        
-    def add_item(self,item):
-        self.__transitions.append(item)
-        
-    
