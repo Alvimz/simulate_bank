0abcc658a9f3324f49289a5c454da3bcdd9814c8
gAlvim <ledyzito@hotmail.com>
2024-09-24 14:24:30 -0300

    Quase pronto

--- account.py
+++ account.py
@@ -1,7 +1,10 @@
+from log import Log
 class Account:
     def __init__(self,name:str) -> None:
         self._name = name
         self._money = 0.0
+        self.log = Log(f'log_{name}.txt')
+        self.log.create_file_log()
         
         
     @property    
@@ -15,18 +18,23 @@ class Account:
     @money.setter
     def money(self,qnt):
         if qnt<=0:
+            self.log.add_entrie('Tentou adicionar{qnt}')
+
             raise ValueError('Não pode ser um valor negativo!')
         self._money= qnt
         
     def deposit(self,qnt:float):
         self.money+=qnt
+        self.log.add_entrie(f'Depositou R${qnt}')
         
     def withdraw(self,qnt:float):
         if qnt>self._money:
+            self.log.add_entrie(ValueError('Saldo insuficiente!'))
             raise ValueError('Saldo insuficiente para resgate!')
         self.money-=qnt
         
     def see_money(self)->float:
+        self.log.add_entrie('viu o saldo da conta!')
         return self._money
     
 if __name__ == '__main__':
--- log.py
+++ log.py
@@ -1,2 +1,22 @@
-class Log():
+import os
+class Log:
+    def __init__(self,filename) -> None:
+        self.filename = filename
+        self.entries = list()
+    
+    def create_file_log(self): 
+        if not os.path.exists(self.filename):
+            with open(self.filename,'w',encoding='utf-8') as file:
+                file.write(f'--Transações--')
+             
+    def add_entrie(self,entry:str):
+        self.entries.append(entry)
+        
+    def save_file(self):
+        with open(self.filename,'a',encoding='utf-8') as file:
+            for item in self.entries:
+                file.write(f'{item}\n')
+            self.entries.clear()
+            
+if __name__ == '__main__':
     ...
