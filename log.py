e6c3462bd85abb0ed49bb6022d1150218039f5fa
gAlvim <ledyzito@hotmail.com>
2024-09-24 15:21:51 -0300

    Fim

--- log.py
+++ log.py
@@ -1,8 +1,10 @@
 import os
+import datetime
 class Log:
     def __init__(self,filename) -> None:
         self.filename = filename
         self.entry = list()
+        self.date_now = None
     
     def create_file_log(self): 
         if not os.path.exists(self.filename):
@@ -20,18 +22,21 @@ class Log:
             self.entry.clear()
         
     def deposit(self,cash:int):
-        self.entry.append(f'Depósito: R${cash}')
+        self.entry.append(f'Depósito: R${cash} - {self.date()}')
         
     def withdraw(self,cash:int):
-        self.entry.append(f'Saque: R${cash}')
+        self.entry.append(f'Saque: R${cash} - {self.date()}')
         
     def see_money(self):
-        self.entry.append('Viu o saldo da conta corrente!')
+        self.entry.append(f'Viu o saldo da conta corrente! {self.date()}')
         
     def try_deposit(self):
-        self.entry.append('Tentou adicionar saldo negativo na conta!')
+        self.entry.append(f'Tentou adicionar saldo negativo na conta! {self.date()}')
         
-    
+    def date(self):
+        self.date_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M')
+        return self.date_now 
+        
         
             
 if __name__ == '__main__':
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Aplicando os conhecimentos em POO em 🐍
+
+
+- 'Aplicativo bancário';
+- Criação de log a cada transação!;
+- Encapsulamento;
